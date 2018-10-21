b9b19125d584d0c66543f4631cfa6b50162a1233
Adria Rodriguez <adria_r_p_@hotmail.com>
2018-10-21 21:40:13 +0200

    Fix bug generate corrupted components register file and remove from VCS

--- componentsRegistered.cpp
+++ componentsRegistered.cpp
@@ -1,10 +1,14 @@
 #include "src/ObjectManager.hpp"
 #include "PrintTextComponent.hpp"
+#include "PrintTextComponent2.hpp"
+
 
 /*
  * This file has been generated. To register a component use macro COMPONENT(NameOfComponentClass)
  * in the definition of the class
  */
 void RegisterComponents() {
-ObjectManager::GetInstance().registerComponentBuilder("PrintTextComponent", new ComponentTBuilder<PrintTextComponent>());
+    ObjectManager::GetInstance().registerComponentBuilder("PrintTextComponent", new ComponentTBuilder<PrintTextComponent>());
+    ObjectManager::GetInstance().registerComponentBuilder("PrintTextComponent2", new ComponentTBuilder<PrintTextComponent2>());
+
 }
--- engine_components_gen.sh
+++ engine_components_gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 components=($(grep -RE --include=\*.hpp '^\s*COMPONENT\(' | sed -r 's/([^:]+):.+\(([^\)]+).+/\1;\2/g'))
 
-NEWLINE=$'\n'
+NEWLINE="\n"
 INCLUDES=""
 REGISTERS=""
 
@@ -11,19 +11,19 @@ for var in "${components[@]}"
 do
 read -ra ADDR <<< "$var"
 INCLUDES="$INCLUDES#include \"${ADDR[0]}\"$NEWLINE"
-REGISTERS="$REGISTERS ObjectManager::GetInstance().registerComponentBuilder(\"${ADDR[1]}\", new ComponentTBuilder<${ADDR[1]}>());$NEWLINE"
+REGISTERS="$REGISTERS    ObjectManager::GetInstance().registerComponentBuilder(\"${ADDR[1]}\", new ComponentTBuilder<${ADDR[1]}>());$NEWLINE"
 done
 IFS=$OIFS
 
 echo "#include \"src/ObjectManager.hpp\""
-echo $INCLUDES
+echo -e $INCLUDES
 echo ""
 echo "/*"
 echo " * This file has been generated. To register a component use macro COMPONENT(NameOfComponentClass)" 
 echo " * in the definition of the class" 
 echo " */" 
 echo "void RegisterComponents() {"
-echo      $REGISTERS
+echo -e "$REGISTERS"
 echo "}"
 
 
