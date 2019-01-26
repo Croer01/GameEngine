3f19fea0336bf18d7dcdf17c293f92d772eb910f
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-01-26 12:01:40 +0100

    Add more test and change some test names

--- test/GameObjectTests.cpp
+++ test/GameObjectTests.cpp
@@ -1,16 +1,24 @@
-//
-// Created by adria on 20/01/2019.
-//
-
 #include "gtest/gtest.h"
 #include <game-engine/geGame.hpp>
 #include <game-engine/geGameObject.hpp>
 
-TEST(GameObjectEvents, transformChange)
+TEST(GameObject, createObject)
 {
     GameEngine::geGame game;
 
     GameEngine::geGameObjectRef go = game.createObject("test1");
 
     ASSERT_EQ(go->name(), "test1");
+}
+
+TEST(GameObject, changeName)
+{
+    const std::string &oldName = "old name";
+    const std::string &newName = "new name";
+    GameEngine::geGame game;
+    GameEngine::geGameObjectRef go = game.createObject(oldName);
+
+    ASSERT_EQ(go->name(), oldName);
+    go->name(newName);
+    ASSERT_EQ(go->name(), newName);
 }
