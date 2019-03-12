6af10d94a93bcd6bfb141755d9c9acc019825aba
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-03-12 23:03:18 +0100

    Refactor SpaceInvaders main

--- main.cpp
+++ main.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
-#include "src/Game.hpp"
-#include "src/GameObject.hpp"
-#include "src/Factory.hpp"
-#include "src/SceneManager.hpp"
-#include "src/ObjectManager.hpp"
+#include <game-engine/geEnvironment.hpp>
+#include <game-engine/geGame.hpp>
+#include "componentsRegistered.hpp"
 
 int main() {
-    try {
-        Game::GetInstance().init("conf");
-        //load object from file
-        ObjectManager::GetInstance().registerPrototype("Alien1", "data/alien1.yaml");
-        ObjectManager::GetInstance().registerPrototype("Alien2", "data/alien2.yaml");
-        ObjectManager::GetInstance().registerPrototype("Alien3", "data/alien3.yaml");
-        ObjectManager::GetInstance().registerPrototype("MotherShip", "data/mothership.yaml");
-        ObjectManager::GetInstance().registerPrototype("Player", "data/player.yaml");
-        ObjectManager::GetInstance().registerPrototype("PlayerBullet", "data/player bullet.yaml");
-        ObjectManager::GetInstance().registerPrototype("EnemyBullet", "data/enemy bullet.yaml");
-        ObjectManager::GetInstance().registerPrototype("EnemyManager", "data/enemyManager.yaml");
-        ObjectManager::GetInstance().registerPrototype("ScoreText", "data/scoreText.yaml");
-        ObjectManager::GetInstance().registerPrototype("StartGameText", "data/startGameText.yaml");
-        ObjectManager::GetInstance().registerPrototype("StartGameScoreText", "data/startGameScoreText.yaml");
-        ObjectManager::GetInstance().registerPrototype("MotherShipStartMenu", "data/motherShipStartMenu.yaml");
-        ObjectManager::GetInstance().registerPrototype("MotherShipScoreText", "data/motherShipScoreText.yaml");
-        ObjectManager::GetInstance().registerPrototype("EnemyExplosion", "data/enemyExplosion.yaml");
-        ObjectManager::GetInstance().registerPrototype("LifesCounter", "data/playerLifesCounter.yaml");
-        ObjectManager::GetInstance().registerPrototype("GameManager", "data/gameManager.yaml");
-        ObjectManager::GetInstance().registerPrototype("Shelter", "data/shelter.yaml");
 
-        //register scene
-        SceneManager::GetInstance().registerScene("StartMenu","data/startMenuScene.yaml");
-        SceneManager::GetInstance().registerScene("Scene0","data/gameplayScene.yaml");
+    GameEngine::geEnvironment env;
+    GameEngine::geGame game;
 
-        SceneManager::GetInstance().changeScene("StartMenu");
-        Game::GetInstance().loop();
+    env.configurationPath("conf");
 
-        return 0;
-    }
-    catch (const std::exception &e){
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
-    catch (...){
-        std::cerr << "unexcepted error" << std::endl;
-        return 1;
-    }
+    RegisterComponents(&env);
+
+    game.init();
+
+    //load object from file
+    env.addPrototype("Alien1", "data/alien1.yaml");
+    env.addPrototype("Alien2", "data/alien2.yaml");
+    env.addPrototype("Alien3", "data/alien3.yaml");
+    env.addPrototype("MotherShip", "data/mothership.yaml");
+    env.addPrototype("Player", "data/player.yaml");
+    env.addPrototype("PlayerBullet", "data/player bullet.yaml");
+    env.addPrototype("EnemyBullet", "data/enemy bullet.yaml");
+    env.addPrototype("EnemyManager", "data/enemyManager.yaml");
+    env.addPrototype("ScoreText", "data/scoreText.yaml");
+    env.addPrototype("StartGameText", "data/startGameText.yaml");
+    env.addPrototype("StartGameScoreText", "data/startGameScoreText.yaml");
+    env.addPrototype("MotherShipStartMenu", "data/motherShipStartMenu.yaml");
+    env.addPrototype("MotherShipScoreText", "data/motherShipScoreText.yaml");
+    env.addPrototype("EnemyExplosion", "data/enemyExplosion.yaml");
+    env.addPrototype("LifesCounter", "data/playerLifesCounter.yaml");
+    env.addPrototype("GameManager", "data/gameManager.yaml");
+    env.addPrototype("Shelter", "data/shelter.yaml");
+
+    //register scene
+    env.addScene("StartMenu", "data/startMenuScene.yaml");
+    env.addScene("Scene0", "data/gameplayScene.yaml");
+
+    env.firstScene("StartMenu");
+
+    game.configEnvironment(env);
+
+    return game.loop();
 }
