ccba825f0068a4887d0d739e3381977a725903ef
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-03-12 22:54:20 +0100

    Refactor component register generator to use new Builder API

--- engine_components_generator.py
+++ engine_components_generator.py
@@ -11,16 +11,16 @@ for filename in glob.iglob("./**/*.hpp", recursive=True):
         match = re.search("^\s*COMPONENT\(([^\)]+)\)", line)
         if match:
             includes.append("#include \"" + os.path.basename(filename) + "\"")
-            registers.append("\tObjectManager::GetInstance().registerComponentBuilder(\"" + match.group(1) + "\", new ComponentTBuilder<" + match.group(1) + ">());")
+            registers.append("\tenv->registerComponent(\"" + match.group(1) + "\", new GameEngine::ComponentTBuilder<" + match.group(1) + ">());")
             break
 
-print("#include \"src/ObjectManager.hpp\"")
+print("#include <game-engine/geEnvironment.hpp>")
 print(*includes, sep="\n")
 print("")
 print("/*")
 print(" * This file has been generated. To register a component use macro COMPONENT(NameOfComponentClass)")
 print(" * in the definition of the class")
 print(" */")
-print("void RegisterComponents() {")
+print("void RegisterComponents(GameEngine::geEnvironment *env) {")
 print(*registers, sep="\n")
-print("}")
+print("};")
