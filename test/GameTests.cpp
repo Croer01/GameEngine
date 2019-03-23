b27464e917b984d08958f0ed57954482bfa51039
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-03-23 20:50:47 +0100

    Change CMake to compile the library and the tests

--- test/GameTests.cpp
+++ test/GameTests.cpp
@@ -60,7 +60,7 @@ TEST(Game, playSound)
     environment.addPrototype(prototype, "data/audioComponentLoadTest.yaml");
 
     std::thread t([&](){
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         auto audioComponent = game.findObjectByNameInCurrentScene(prototype)->getComponent<GameEngine::AudioComponent>();
 
         if(auto audioComponentShared = audioComponent.lock()){
