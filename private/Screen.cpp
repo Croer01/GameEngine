b5b98042d3bf8a5da82d4f5f3f441bd1b3845a70
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-05-04 22:34:32 +0200

    Change GL minor version to use RenderDoc to debug
    https://renderdoc.org/

--- private/Screen.cpp
+++ private/Screen.cpp
@@ -193,7 +193,7 @@ namespace Internal {
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #endif
 
         // Create window with graphics context
