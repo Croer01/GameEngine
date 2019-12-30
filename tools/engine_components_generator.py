5bc1b04bc6d320742425e78ea86ce932a19f9570
Adria Rodriguez <adria_r_p_@hotmail.com>
2019-12-30 18:45:27 +0100

    Component register script generates properties
    Also the script get the files to process from the command arguments
    instead of search internally

--- tools/engine_components_generator.py
+++ tools/engine_components_generator.py
@@ -1,18 +1,29 @@
 import os
-import glob
+import sys
 import re
 
 # global vars
 includes = []
-registers = []
+propertiesRegisters = []
+componentRegisters = []
 
-for filename in glob.iglob("./**/*.hpp", recursive=True):
+# pre-condition: all the arguments from command are files
+for filename in sys.argv[1:]:
     for line in open(filename, "r"):
+        include = None
+        # register a component
         match = re.search("^\s*COMPONENT\(([^\)]+)\)", line)
         if match:
-            includes.append("#include \"" + os.path.basename(filename) + "\"")
-            registers.append("\tenv->registerComponent(\"" + match.group(1) + "\", new GameEngine::ComponentTBuilder<" + match.group(1) + ">());")
-            break
+            include = "#include \"" + os.path.basename(filename) + "\""
+            componentRegisters.append("\tenv->registerComponent(\"" + match.group(1) + "\", new GameEngine::ComponentTBuilder<" + match.group(1) + ">());")
+        # register a property
+        match = re.search("^\s*PROPERTIES\(([^\)]+)\)", line)
+        if match:
+            include = "#include \"" + os.path.basename(filename) + "\""
+            propertiesRegisters.append("\tenv->registerProperties(\"" + match.group(1) + "\", new " + match.group(1) + "());")
+
+        if include is not None:
+            includes.append(include)
 
 print("#ifndef COMPONENT_REGISTERS_GENERATED_HPP")
 print("#define COMPONENT_REGISTERS_GENERATED_HPP")
@@ -24,6 +35,7 @@ print(" * This file has been generated. To register a component use macro COMPON
 print(" * in the definition of the class")
 print(" */")
 print("void RegisterComponents(GameEngine::geEnvironment *env) {")
-print(*registers, sep="\n")
+print(*propertiesRegisters, sep="\n")
+print(*componentRegisters, sep="\n")
 print("};")
 print("#endif")
