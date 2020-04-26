a72aa38278aa94f18991925e7f7e10b249bb2190
Adria Rodriguez <adria_r_p_@hotmail.com>
2020-04-26 15:11:18 +0200

    Rename pathfinding's Actor to Agent

--- engine/private/pathfinding/Actor.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by adria on 22/04/2020.
-//
-
-#include "Actor.hpp"
--- engine/private/pathfinding/Agent.hpp
+++ engine/private/pathfinding/Agent.hpp
@@ -6,20 +6,26 @@
 #define GAMEENGINE_ACTOR_HPP
 
 
+#include <memory>
 #include <game-engine/api.hpp>
 #include <game-engine/events/Subject.hpp>
 
 namespace GameEngine {
 namespace Internal {
 
-enum class ActorEvents{
+enum class AgentEvents{
     MOVING
 };
 
-class Actor : Subject<ActorEvents>
+class Agent;
+typedef std::shared_ptr<Agent> AgentRef;
+
+class Agent : public Subject<AgentEvents>
 {
     Vec2D position_;
+public:
     void setPosition(const Vec2D &position);
+    Vec2D getPosition() const;
 };
 }
 }
