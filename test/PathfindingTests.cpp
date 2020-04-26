9211a54562d03757aad24cd1f4c2085f306abd7e
Adria Rodriguez <adria_r_p_@hotmail.com>
2020-04-26 15:12:47 +0200

    Create tests to check pathfinding's agent
    Also add ASCII art to be more easy to understant the path is used in
    every pathfinding test

--- test/PathfindingTests.cpp
+++ test/PathfindingTests.cpp
@@ -25,7 +25,7 @@ void testPath(const std::vector<Vec2D> &expected)
 {
     std::unique_ptr<AStarPathfinding> pathfinder_ =std::make_unique<AStarPathfinding>(5,5);
 
-    const Path &path = pathfinder_->findPath(expected[0], expected[4]);
+    const Path &path = pathfinder_->findPath(expected[0], expected[expected.size()-1]);
     EXPECT_THAT(path.cells_, ElementsAreArray(expected));
 }
 
@@ -34,18 +34,45 @@ void testPathWithModifiers(const std::vector<Vec2D> &expected, const Vec2D &minM
     std::unique_ptr<AStarPathfinding> pathfinder_ =std::make_unique<AStarPathfinding>(5,5);
 
     pathfinder_->setModifier(minModifierPos, maxModifierPos, modifier);
-    const Path &path = pathfinder_->findPath(expected[0], expected[4]);
+    const Path &path = pathfinder_->findPath(expected[0], expected[expected.size()-1]);
+    EXPECT_THAT(path.cells_, ElementsAreArray(expected));
+}
+
+void testPathWithAgents(const std::vector<Vec2D> &expected, const std::vector<Vec2D> &agentsPos)
+{
+    std::unique_ptr<AStarPathfinding> pathfinder_ =std::make_unique<AStarPathfinding>(5,5);
+
+    for(auto pos : agentsPos)
+    {
+        AgentRef agent = std::make_shared<Agent>();
+        agent->setPosition(pos);
+        pathfinder_->addAgent(agent);
+    }
+
+    const Path &path = pathfinder_->findPath(expected[0], expected[expected.size()-1]);
     EXPECT_THAT(path.cells_, ElementsAreArray(expected));
 }
 
 TEST(Pathfinding, crossRightLeftAndVerticalCenter)
 {
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |OO|--|--|--|DD|
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(0,2),
         Vec2D(1,2),
         Vec2D(2,2),
         Vec2D(3,2),
-        Vec2D(4,2),
+        Vec2D(4,2)
     };
     testPath(expectedPath);
 }
@@ -53,6 +80,18 @@ TEST(Pathfinding, crossRightLeftAndVerticalCenter)
 
 TEST(Pathfinding, crossUpDownAndHorizontalCenter)
 {
+//  +--+--+--+--+--+
+//  |  |  |OO|  |  |
+//  +--+--+--+--+--+
+//  |  |  ||||  |  |
+//  +--+--+--+--+--+
+//  |  |  ||||  |  |
+//  +--+--+--+--+--+
+//  |  |  ||||  |  |
+//  +--+--+--+--+--+
+//  |  |  |DD|  |  |
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(2,0),
         Vec2D(2,1),
@@ -66,6 +105,18 @@ TEST(Pathfinding, crossUpDownAndHorizontalCenter)
 
 TEST(Pathfinding, FromUpperLeftToBottomRight)
 {
+//  +--+--+--+--+--+
+//  |OO|  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |\\|  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |\\|  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |\\|  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |DD|
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(0,0),
         Vec2D(1,1),
@@ -80,6 +131,18 @@ TEST(Pathfinding, FromUpperLeftToBottomRight)
 
 TEST(Pathfinding, FromUpperRightToBottomLeft)
 {
+//  +--+--+--+--+--+
+//  |  |  |  |  |OO|
+//  +--+--+--+--+--+
+//  |  |  |  |//|  |
+//  +--+--+--+--+--+
+//  |  |  |//|  |  |
+//  +--+--+--+--+--+
+//  |  |//|  |  |  |
+//  +--+--+--+--+--+
+//  |DD|  |  |  |  |
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(4,0),
         Vec2D(3,1),
@@ -93,12 +156,24 @@ TEST(Pathfinding, FromUpperRightToBottomLeft)
 
 TEST(Pathfinding, crossRightLeftAndVerticalCenterMiddleModified)
 {
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |--|  |  |
+//  +--+--+--+--+--+
+//  |OO|//|MM|\\|DD|
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(0,2),
         Vec2D(1,2),
         Vec2D(2,1),
         Vec2D(3,2),
-        Vec2D(4,2),
+        Vec2D(4,2)
     };
     testPathWithModifiers(expectedPath,Vec2D(2,2),Vec2D(2,2),10);
 }
@@ -106,12 +181,112 @@ TEST(Pathfinding, crossRightLeftAndVerticalCenterMiddleModified)
 
 TEST(Pathfinding, crossRightLeftPlusOneAndVerticalCenterMiddleModified)
 {
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |--|  |  |
+//  +--+--+--+--+--+
+//  |OO|//|MM|\\|  |
+//  +--+--+--+--+--+
+//  |  |  |MM|  |DD|
+//  +--+--+--+--+--+
+//  |  |  |MM|  |  |
+//  +--+--+--+--+--+
+
     std::vector<Vec2D> expectedPath = {
         Vec2D(0,2),
         Vec2D(1,2),
         Vec2D(2,1),
         Vec2D(3,2),
-        Vec2D(4,3),
+        Vec2D(4,3)
     };
     testPathWithModifiers(expectedPath,Vec2D(2,2),Vec2D(2,4),10);
+}
+
+TEST(Pathfinding, checkAgentsBlockCells)
+{
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |--|--|  |
+//  +--+--+--+--+--+
+//  |  |//|AA|AA|\\|
+//  +--+--+--+--+--+
+//  |OO|  |AA|AA|DD|
+//  +--+--+--+--+--+
+
+    std::vector<Vec2D> expectedPath = {
+        Vec2D(0,4),
+        Vec2D(1,3),
+        Vec2D(2,2),
+        Vec2D(3,2),
+        Vec2D(4,3),
+        Vec2D(4,4)
+    };
+
+    std::vector<Vec2D> agents = {
+        Vec2D(2,4),
+        Vec2D(2,3),
+        Vec2D(3,3),
+        Vec2D(3,4)
+    };
+    testPathWithAgents(expectedPath, agents);
+}
+
+TEST(Pathfinding, checkAgentMovingBlockCells)
+{
+//  1st path
+//  +--+--+--+--+--+
+//  |AA|  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |OO|--|--|--|DD|
+//  +--+--+--+--+--+
+    std::vector<Vec2D> expectedFirstPath = {
+        Vec2D(0,4),
+        Vec2D(1,4),
+        Vec2D(2,4),
+        Vec2D(3,4),
+        Vec2D(4,4)
+    };
+//  2nd path
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |  |  |
+//  +--+--+--+--+--+
+//  |  |  |  |--|  |
+//  +--+--+--+--+--+
+//  |OO|--|//|AA|DD|
+//  +--+--+--+--+--+
+    std::vector<Vec2D> expectedSecondPath = {
+        Vec2D(0,4),
+        Vec2D(1,4),
+        Vec2D(2,4),
+        Vec2D(3,3),
+        Vec2D(4,4)
+    };
+
+    std::unique_ptr<AStarPathfinding> pathfinder_ =std::make_unique<AStarPathfinding>(5,5);
+    AgentRef agent = std::make_shared<Agent>();
+    agent->setPosition(Vec2D(0,0));
+    pathfinder_->addAgent(agent);
+
+    // check first path
+    const Path &firstPath = pathfinder_->findPath(expectedFirstPath[0], expectedFirstPath[4]);
+    EXPECT_THAT(firstPath.cells_, ElementsAreArray(expectedFirstPath));
+
+    // check second path
+    agent->setPosition(Vec2D(3,4));
+    const Path &secondPath = pathfinder_->findPath(expectedSecondPath[0], expectedSecondPath[4]);
+    EXPECT_THAT(secondPath.cells_, ElementsAreArray(expectedSecondPath));
 }
