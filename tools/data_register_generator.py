b310cf09e8eb5ebd6d38109eab17ef0d2f92dab1
Adria Rodriguez <adria_r_p_@hotmail.com>
2020-06-20 11:51:16 +0200

    Change Data Register Generator to use new api

--- tools/data_register_generator.py
+++ tools/data_register_generator.py
@@ -16,22 +16,24 @@ for filename in dataFiles:
     dataObjectName = filename.stem
 
     if ".scene" == filename.suffix:
-        sceneRegisters.append("\tenv->addScene(\"" + dataObjectName + "\", \"" + filename.as_posix() + "\");")
+        sceneRegisters.append("\tenv->addScene(\"" + dataObjectName + "\", \"" + filename.relative_to(dataFolderPath).as_posix() + "\");")
     elif ".prototype" == filename.suffix:
-        prototypeRegisters.append("\tenv->addPrototype(\"" + dataObjectName + "\", \"" + filename.as_posix() + "\");")
+        prototypeRegisters.append("\tenv->addPrototype(\"" + dataObjectName + "\", \"" + filename.relative_to(dataFolderPath).as_posix() + "\");")
     else:
         raise ValueError("unkown file format " + filename.suffix)
 
+
+dataFolderString = dataFolderPath.resolve().as_posix()
 print("#ifndef DATA_REGISTERS_GENERATED_HPP")
 print("#define DATA_REGISTERS_GENERATED_HPP")
 print("#include <game-engine/geEnvironment.hpp>")
 print("")
 print("/*")
 print(" * This file has been generated based in the content of data folder")
-print(" * data folder: " + str(dataFolderPath))
-print(" * cwd: " + str(os.getcwd()))
+print(" * data folder: " + dataFolderString)
 print(" */")
 print("void inline RegisterData(const GameEngine::geEnvironmentRef &env) {")
+print("    env->dataPath(\"" + dataFolderString + "\");")
 print(*prototypeRegisters, sep="\n")
 print(*sceneRegisters, sep="\n")
 print("};")
