8a8ee326c46320d8e8273eb9832492c056062698
Adria Rodriguez <adria_r_p_@hotmail.com>
2021-01-10 11:44:00 +0100

    Update minor GL ver. in editor to be able to debug
    Debug using RenderDoc

--- editor/editorMain.cpp
+++ editor/editorMain.cpp
@@ -58,7 +58,7 @@ int main(int, char**)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #endif
 
     // Create window with graphics context
