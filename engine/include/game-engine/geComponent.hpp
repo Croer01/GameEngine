0f2af8bc89ad5bba5da7d11fbafc8e0e6a522278
Adria Rodriguez <adria_r_p_@hotmail.com>
2022-07-27 18:02:40 +0200

    Refactor Component base class to support DLL
    With the old class structure, it wasn't possible to create components
    using the InstantiableComponent class.
    
    Currently, I add a macro as an intermediate solution to check all it
    works as the beginning of this refactor.

--- engine/include/game-engine/geComponent.hpp
+++ engine/include/game-engine/geComponent.hpp
@@ -20,6 +20,7 @@ namespace GameEngine {
 
     class PUBLICAPI geComponent {
         GameEngine::geGameObject *gameObject_;
+        std::shared_ptr<ComponentData> componentData_;
     protected:
         virtual void onGameObjectChange(GameEngine::geGameObject *oldGameObject, GameEngine::geGameObject *newGameObject) {};
         virtual geComponentRef instantiate() const = 0;
@@ -31,109 +32,120 @@ namespace GameEngine {
         void gameObject(geGameObject *gameObject);
         geGameObject *gameObject() const;
         virtual geComponentRef clone() const = 0;
-        virtual void setData(const std::shared_ptr<ComponentData> data) = 0;
-        virtual ComponentData *getData() const = 0;
         virtual ComponentDataRef instantiateData() const = 0;
-    };
-
-    /**
-     * This is a helper class that implement the boiler plate code to instantiate a class derived from \class geComponent.
-     * This is only for the first inheritance level, if you derive a class currently derived from \class geComponentInstantiable,
-     * you must implement the methods as this class implements: instantiate, clone and getProperties.
-     * */
-    template<typename CompT, typename CompDataT>
-    class PUBLICAPI geComponentInstantiable : public geComponent
-    {
-        std::shared_ptr<ComponentData> componentData_;
-    protected:
-        geComponentRef instantiate() const override
-        {
-            auto instance = std::make_shared<CompT>();
-            return instance;
-        }
-
-        virtual geComponentRef clone() const {
-            geComponentRef cloned = instantiate();
-            cloned->setData(getData()->template clone<CompDataT>());
-
-            return cloned;
-        }
-
-    public:
-        virtual ~geComponentInstantiable(){};
-
-        virtual void setData(const std::shared_ptr<ComponentData> data)
+        void setData(const std::shared_ptr<ComponentData> data)
         {
             componentData_ = data;
         };
-
-        virtual ComponentData *getData() const
+        ComponentData *getData() const
         {
             return componentData_.get();
         };
 
-        virtual ComponentDataRef instantiateData() const
-        {
-            auto instance = std::make_shared<CompDataT>();
-            return instance;
-        }
+        // HELPER METHODS
 
         template<typename ValueType>
         void setPropertyValue(const std::string &name, const ValueType &value)
         {
-            assert(componentData_);
-            componentData_->template getProperty<ValueType>(name)->set(value);
+            ComponentData *data = getData();
+            assert(data);
+            data->template getProperty<ValueType>(name)->set(value);
         }
 
         template<typename ValueType>
         ValueType &getPropertyValue(const std::string &name) const
         {
-            assert(componentData_);
-            return componentData_->template getProperty<ValueType>(name)->get();
+            ComponentData *data = getData();
+            assert(data);
+            return data->template getProperty<ValueType>(name)->get();
         }
 
         template<typename ValueType>
         void setPropertyObserver(const std::string &name, const std::function<void()> &callback)
         {
-            assert(componentData_);
-            componentData_->template getProperty<ValueType>(name)->registerObserver(callback);
+            ComponentData *data = getData();
+            assert(data);
+            data->template getProperty<ValueType>(name)->registerObserver(callback);
         }
 
         void setFilePathPropertyValue(const std::string &name, const std::string &value)
         {
-            assert(componentData_);
-            componentData_->getFilePathProperty(name)->set(value);
+            ComponentData *data = getData();
+            assert(data);
+            data->getFilePathProperty(name)->set(value);
         }
 
         std::string &getFilePathPropertyValue(const std::string &name) const
         {
-            assert(componentData_);
-            return componentData_->getFilePathProperty(name)->get();
+            ComponentData *data = getData();
+            assert(data);
+            return data->getFilePathProperty(name)->get();
         }
 
         void setFilePathPropertyObserver(const std::string &name, const std::function<void()> &callback)
         {
-            assert(componentData_);
-            componentData_->getFilePathProperty(name)->registerObserver(callback);
+            ComponentData *data = getData();
+            assert(data);
+            data->getFilePathProperty(name)->registerObserver(callback);
         }
 
         void setEnumPropertyValue(const std::string &name, const std::string &value)
         {
-            assert(componentData_);
-            componentData_->getEnumProperty(name)->set(value);
+            ComponentData *data = getData();
+            assert(data);
+            data->getEnumProperty(name)->set(value);
         }
 
         std::string &getEnumPropertyValue(const std::string &name) const
         {
-            assert(componentData_);
-            return componentData_->getEnumProperty(name)->get();
+            ComponentData *data = getData();
+            assert(data);
+            return data->getEnumProperty(name)->get();
         }
 
         void setEnumPropertyObserver(const std::string &name, const std::function<void()> &callback)
         {
-            assert(componentData_);
-            componentData_->getEnumProperty(name)->registerObserver(callback);
+            ComponentData *data = getData();
+            assert(data);
+            data->getEnumProperty(name)->registerObserver(callback);
         }
+    };
+
+    /**
+     * This is a helper class that implement the boiler plate code to instantiate a class derived from \class geComponent.
+     * This is only for the first inheritance level, if you derive a class currently derived from \class geComponentInstantiable,
+     * you must implement the methods as this class implements: instantiate, clone and instantiateData.
+     * */
+    
+    #define COMPONENT_BOILERPLATE(CompT, CompDataT)                 \
+    protected:                                                      \
+        geComponentRef instantiate() const override                 \
+        {                                                           \
+            auto instance = std::make_shared<CompT>();              \
+            return instance;                                        \
+        };                                                          \
+                                                                    \
+        virtual geComponentRef clone() const {                      \
+            geComponentRef cloned = instantiate();                  \
+            cloned->setData(getData()->template clone<CompDataT>());\
+                                                                    \
+            return cloned;                                          \
+        };                                                          \
+                                                                    \
+    public:                                                         \
+        virtual ComponentDataRef instantiateData() const            \
+        {                                                           \
+            auto instance = std::make_shared<CompDataT>();          \
+            return instance;                                        \
+        };
+
+
+    template<typename CompT, typename CompDataT>
+    class PUBLICAPI geComponentInstantiable : public geComponent
+    {
+        COMPONENT_BOILERPLATE(CompT, CompDataT)
+        virtual ~geComponentInstantiable(){};
+
     };
 }
 #endif //SPACEINVADERS_GECOMPONENT_HPP
